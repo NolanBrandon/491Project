68e41fa759318738e29a4ba1fe84b11de12d4fee
NolanBrandon <89492670+NolanBrandon@users.noreply.github.com>
2025-09-14 19:55:16 -0700

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://wzgxtvqkivbiinkoewmf.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6Z3h0dnFraXZiaWlua29ld21mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NTM4MDEsImV4cCI6MjA3MzAyOTgwMX0.N4EcHLKB1EXyUVUTf3OhlJ6P-bss2C0A5xPvt7GQ3VE"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
